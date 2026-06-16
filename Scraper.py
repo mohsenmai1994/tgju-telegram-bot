855f3f1242394f05a1d5d02dbdd146afb5595737
mohsenmai1994 <mohsen.m.ai1994@gmail.com>
2026-06-16 17:48:18 +0330

    Update Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -28,6 +28,7 @@ class __webdriver__(webdriver.Chrome):
             options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36')
             # Disable the "AutomationControlled" flag
             options.add_argument('--disable-blink-features=AutomationControlled')
+            options.add_argument('--dns-prefetch-disable')
 
         super().__init__(options=options, service=service, keep_alive=keep_alive)
         self.set_window_size(2560, 1440)
